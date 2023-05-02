106239c97c96bb03d6c21c70241b8508d41740ce
Adarsh Urs <adarsh@vlcmobileremote.com>
2023-05-02 13:03:55 +0530

    Updated VLCServer Class

--- lib/model/vlc_server.dart
+++ lib/model/vlc_server.dart
@@ -1,6 +1,15 @@
 class VLCServer {
   //late String name;
+  late String name;
   late String ipAddress;
   String? vlcPassword;
-  String vlcPort = "8080";
+  String vlcPort;
+
+  bool isDefault = false;
+  String vlcHomeFolderPath = "uri=file%3A%2F%2F%2F";
+  bool isPreviouslyConnected = false;
+
+  VLCServer({required this.ipAddress, this.vlcPort = "8080", required this.vlcPassword});
   }
+
+  
