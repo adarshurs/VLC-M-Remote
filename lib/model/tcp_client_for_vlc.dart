a8b77725993fa0141a7ef923b40eed93c6462d53
Adarsh Urs <adarsh@vlcmobileremote.com>
2023-05-02 12:20:22 +0530

    created vlc_m_remote package Implemented vlc status fetching

---
+++ lib/model/tcp_client_for_vlc.dart
@@ -0,0 +1,159 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:vlc_m_remote/model/vlc_status.dart';
+import 'package:vlc_m_remote/model/vlc_status_response.dart';
+
+class TCPClientForVLC {
+  final String ipAddress;
+  late final String _password;
+  final String vlcPort;
+  final String _vlcStatusPath = "requests/status.json?";
+  Socket? _socket;
+
+  Timer? _getVLCStatusTimer;
+  final Duration _statusUpdateInterval = const Duration(milliseconds: 950);
+  bool _keepTryingToConnect = false;
+
+  StreamController? _streamController;
+  Stream? get stream => _streamController?.stream;
+
+  int _reTryFetchingCounter = 0;
+  VLCStatusResponse _vlcStatusResponse = VLCStatusResponse(vlcStatus: VLCStatus(), errorMessage: '');
+
+  TCPClientForVLC(
+      {required this.ipAddress,
+      this.vlcPort = "8080",
+      required String vlcPassword}) {
+    _password = base64Url.encode(utf8.encode("" + ":" + vlcPassword));
+    _keepTryingToConnect = true;
+    _reTryFetchingCounter = 0;
+    _vlcStatusResponse = VLCStatusResponse(vlcStatus: VLCStatus(), errorMessage: "");
+    _streamController = StreamController<VLCStatusResponse>(onListen: () {
+      startFetchingVLCStatus();
+    }, onCancel: () {
+      stopFetchingVLCStatus();
+    }, onPause: () {
+      stopFetchingVLCStatus();
+    }, onResume: () {
+      stopFetchingVLCStatus();
+    });
+//    startFetchingVLCStatus();
+  }
+
+  startFetchingVLCStatus() async {
+    //int counter = 0;
+
+    _getVLCStatusTimer = null;
+    await connectToVLC().then((isConnected) async {
+      if (isConnected) {
+        //print("Socket Connected");
+        sendRequestToVLC(null);
+        _getVLCStatusTimer = Timer.periodic(_statusUpdateInterval, (timer) {
+          sendRequestToVLC(null);
+          //counter++;
+          //if (counter > 10) {
+          //stopFetchingVLCStatus();
+          //}
+        });
+      } else {
+        if (_keepTryingToConnect) {
+          await Future.delayed(const Duration(seconds: 1));
+          //print("Socket retrying connection");
+          startFetchingVLCStatus();
+        }
+      }
+    });
+  }
+
+
+  stopFetchingVLCStatus() {
+    _keepTryingToConnect = false;
+    _getVLCStatusTimer?.cancel();
+    _socket?.close();
+  }
+
+  
+
+  _reTryFetching() async {
+    _getVLCStatusTimer?.cancel();
+    _socket?.close();
+    if (_keepTryingToConnect) {
+      await Future.delayed(const Duration(seconds: 1));
+      startFetchingVLCStatus();
+      _reTryFetchingCounter++;
+
+      if (_reTryFetchingCounter >= 5) {
+        _vlcStatusResponse.errorMessage = "VLCNotFound";
+        _streamController?.sink.add(_vlcStatusResponse);
+        _reTryFetchingCounter = 0;
+      }
+    }
+  }
+
+  
+
+  _onDataReceived(String dataReceived) {
+    //print("dataReceived");
+    int index = dataReceived.indexOf("{");
+    //print(index);
+    if (index != -1) {
+      var body = dataReceived.substring(index);
+      // print(body);
+      try{
+      VLCStatus vlcStatus = VLCStatus.fromJson(jsonDecode(body));
+      _vlcStatusResponse =
+          VLCStatusResponse(vlcStatus: vlcStatus, errorMessage: "");
+
+      if (_streamController != null && !_streamController!.isClosed) {
+        _streamController?.sink.add(_vlcStatusResponse);
+      }
+      } catch(e){
+        print(e.toString());
+      }
+    } else {
+      // print(dataReceived);
+    }
+  }
+
+  Future<bool> connectToVLC() async {
+    bool isConnected = false;
+    try {
+      _socket = await Socket.connect(ipAddress, int.parse(vlcPort),
+          timeout: const Duration(milliseconds: 2000));
+      isConnected = true;
+      _socket!.listen((event) {
+        _onDataReceived(utf8.decode(event));
+      }, onError: (error) {
+        //This may not get called all the time
+        //print(error);
+      }, onDone: () {
+        //This will get called when socket gets closed due to error, so retrying connection from here
+        _reTryFetching();
+        //print("Socket Closed");
+      });
+    } catch (e) {
+      isConnected = false;
+    }
+    return isConnected;
+  }
+
+  List<int> _getRequestHeader(String requestToSend) {
+    String data = "GET /$requestToSend HTTP/1.1\r\n";
+    List<List<String>> requestHeaders = [
+      ["Authorization", "Basic $_password"]
+    ];
+    for (var element in requestHeaders) {
+      data += "${element[0]}:${element[1]}\r\n";
+    }
+    data += "\r\n";
+    return utf8.encode(data);
+  }
+
+  //passing a null to requestToSend, gets the vlc status
+  sendRequestToVLC(String? requestToSend) {
+    print("Socket send");
+    _socket?.add(_getRequestHeader(requestToSend ?? _vlcStatusPath));
+  }
+}
