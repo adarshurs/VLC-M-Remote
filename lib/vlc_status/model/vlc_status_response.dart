87dd6cd592c178ac39c2b5813f86474277541a02
Adarsh Urs <adarsh@vlcmobileremote.com>
2023-05-10 16:24:07 +0530

    Added folder structure for vlc_browse and vlc_playlist

---
+++ lib/vlc_status/model/vlc_status_response.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:vlc_m_remote/vlc_status/model/vlc_status.dart';
+
+class VLCStatusResponse {
+  VLCStatus vlcStatus;
+  String errorMessage;
+  VLCStatusResponse({
+    required this.vlcStatus,
+    required this.errorMessage,
+  });
+  //VLCStatusResponse(this.vlcStatus, this.errorMessage);
+
+  // VLCStatusResponse copyWith({
+  //   VLCStatus? vlcStatus,
+  //   String? errorMessage,
+  // }) {
+  //   return VLCStatusResponse(
+  //     vlcStatus: vlcStatus ?? this.vlcStatus,
+  //     errorMessage: errorMessage ?? this.errorMessage,
+  //   );
+  // }
+}
