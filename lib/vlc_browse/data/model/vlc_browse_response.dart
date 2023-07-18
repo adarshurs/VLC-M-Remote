e584c087465471f05e3f6b94a1d292697a147383
Adarsh Urs <5429221+adarshurs@users.noreply.github.com>
2023-07-18 17:04:04 +0530

    refactored

--- lib/vlc_browse/data/model/vlc_browse_response.dart
+++ lib/vlc_browse/data/model/vlc_browse_response.dart
@@ -2,6 +2,7 @@ import 'package:vlc_m_remote/vlc_browse/data/model/vlc_browse_item.dart';
 
 class VlcBrowseResponse {
   List<VlcBrowseItem>? folders;
+  VlcBrowseItem? backNavigationFolderItem;
   String? errorMessage;
   bool showAllFiles = false;
 
@@ -64,10 +65,79 @@ class VlcBrowseResponse {
   ];
 
   VlcBrowseResponse.fromJson(Map<String, dynamic> json) {
+    // if (json['element'] != null) {
+    //   folders = <VlcBrowseItem>[];
+    //   errorMessage = null;
+    //   json['element'].forEach((v) {
+    //     var browseItemTemp = VlcBrowseItem.fromJson(v);
+    //     if (browseItemTemp.type == "file") {
+    //       var fileExtension = browseItemTemp.name
+    //           ?.substring(browseItemTemp.name?.lastIndexOf(".") ?? 0);
+    //       if (fileExtension == ".srt") {
+    //         browseItemTemp.type = "subtitle";
+    //       }
+    //       // if (!showAllFiles) {
+    //       //   if (vlcSupportedFileExtensions.contains(fileExtension)) {
+    //       //     folders!.add(browseItemTemp);
+    //       //   }
+    //       // } else {
+    //       //   folders!.add(browseItemTemp);
+    //       // }
+    //
+    //       if (showAllFiles ||
+    //           vlcSupportedFileExtensions.contains(fileExtension)) {
+    //         folders!.add(browseItemTemp);
+    //       }
+    //     } else {
+    //       folders!.add(browseItemTemp);
+    //     }
+    //   });
+    //
+    //   folders?.sort((a, b) {
+    //     return a.name
+    //         .toString()
+    //         .toLowerCase()
+    //         .compareTo(b.name.toString().toLowerCase());
+    //   });
+    //
+    //   folders?.sort((a, b) {
+    //     return a.type
+    //         .toString()
+    //         .toLowerCase()
+    //         .compareTo(b.type.toString().toLowerCase());
+    //   });
+    //
+    //   if (folders != null) {
+    //     int indexOfBackPathFolder = -1;
+    //     for (VlcBrowseItem element in folders!) {
+    //       indexOfBackPathFolder++;
+    //       var index = element.path?.lastIndexOf("/..") ?? -1;
+    //       if (index != -1) {
+    //         var backDirectoryPath = element.path
+    //             ?.substring(0, element.path?.lastIndexOf("/..") ?? 0);
+    //         print(backDirectoryPath);
+    //         break;
+    //       }
+    //     }
+    //
+    //     if (indexOfBackPathFolder >= 0) {
+    //       backNavigationFolderItem = null;
+    //       backNavigationFolderItem = folders?.elementAt(indexOfBackPathFolder);
+    //       if (backNavigationFolderItem != null) {
+    //         folders?.removeAt(indexOfBackPathFolder);
+    //         //folders?.insert(0, backNavigationFolderItem!);
+    //       }
+    //     }
+    //   }
+    // } else {
+    //   errorMessage = "Error";
+    // }
+
     if (json['element'] != null) {
       folders = <VlcBrowseItem>[];
       errorMessage = null;
-      json['element'].forEach((v) {
+
+      for (var v in json['element']) {
         var browseItemTemp = VlcBrowseItem.fromJson(v);
         if (browseItemTemp.type == "file") {
           var fileExtension = browseItemTemp.name
@@ -75,13 +145,6 @@ class VlcBrowseResponse {
           if (fileExtension == ".srt") {
             browseItemTemp.type = "subtitle";
           }
-          // if (!showAllFiles) {
-          //   if (vlcSupportedFileExtensions.contains(fileExtension)) {
-          //     folders!.add(browseItemTemp);
-          //   }
-          // } else {
-          //   folders!.add(browseItemTemp);
-          // }
 
           if (showAllFiles ||
               vlcSupportedFileExtensions.contains(fileExtension)) {
@@ -90,29 +153,36 @@ class VlcBrowseResponse {
         } else {
           folders!.add(browseItemTemp);
         }
-      });
+      }
 
-      folders?.sort((a, b) {
-        return a.name
-            .toString()
-            .toLowerCase()
-            .compareTo(b.name.toString().toLowerCase());
-      });
+      folders?.sort((a, b) => a.name
+          .toString()
+          .toLowerCase()
+          .compareTo(b.name.toString().toLowerCase()));
+      folders?.sort((a, b) => a.type
+          .toString()
+          .toLowerCase()
+          .compareTo(b.type.toString().toLowerCase()));
 
-      folders?.sort((a, b) {
-        return a.type
-            .toString()
-            .toLowerCase()
-            .compareTo(b.type.toString().toLowerCase());
-      });
+      if (folders != null) {
+        int indexOfBackPathFolder = folders!
+            .indexWhere((element) => element.path?.contains('/..') ?? false);
+        if (indexOfBackPathFolder != -1) {
+          var backDirectoryPath = folders![indexOfBackPathFolder]
+              .path
+              ?.substring(
+                  0,
+                  folders![indexOfBackPathFolder].path!.lastIndexOf('/..') ??
+                      0);
+          print(backDirectoryPath);
+        }
 
-      var backDirectoryPath = folders?[0]
-          .path
-          ?.substring(0, folders?[0].path?.lastIndexOf("/..") ?? 0);
-
-      print(backDirectoryPath);
-
-      //folders = folders?..reversed.toList();
+        if (indexOfBackPathFolder >= 0) {
+          backNavigationFolderItem = null;
+          backNavigationFolderItem = folders?.elementAt(indexOfBackPathFolder);
+          folders?.removeAt(indexOfBackPathFolder);
+        }
+      }
     } else {
       errorMessage = "Error";
     }
